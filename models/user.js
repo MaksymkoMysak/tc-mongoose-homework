01b6a02ebbee5173d30db58c318865f36d7a8e3e
Maksym Mysak <max.mysak2001@gmail.com>
2022-07-12 19:45:41 +0300

    my mongoose homework

--- models/user.js
+++ models/user.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
-const ProductSchema = new Schema({});
+const UserSchema = new Schema({
+  firstName: {type: String, min: 4, max: 50, required: true},
+  lastName: {type: String, min: 3, max: 60, required: true},
+  role: {
+    type: String,
+    enum: ['admin', 'writer', 'quest']
+  },
+  createdAt: {type: Date, default: new Date()},
+  numberOfArticles: {type: Number, default: 0},
+  articles: [{type: ObjectId, ref: 'Article'}],
+  nickname: {type: String}
+});
 
-module.exports = mongoose.model('User', ProductSchema);
+module.exports = mongoose.model('User', UserSchema);
